0e24fef9c2187cdcd555f78868915e56fbbbcf46
Mathew Deal <datmeal@gmail.com>
2022-03-26 06:31:11 +0900

    Added confirmation dialog to deleting characters

--- src/CharacterSelect.js
+++ src/CharacterSelect.js
@@ -1,17 +1,18 @@
 import * as React from "react";
 import Autocomplete from "@mui/material/Autocomplete";
-import FormControl from "@mui/material/FormControl";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
-import { ChromeReaderModeSharp } from "@mui/icons-material";
-
-import icon_deathblade from "./img/icon_deathblade.png";
 
 const classes = [
   { label: "None", id: 0 },
@@ -40,54 +41,83 @@ export default function CharacterSelect(props) {
   const useStore = props.useStore;
   const updateClass = useStore((state) => state.updateClass);
   const removeCharacter = useStore((state) => state.removeCharacter);
+  const [openDialog, setOpenDialog] = React.useState(false);
   const characterName = charData.class;
+
+  function handleOpenDialog() {
+    setOpenDialog(true);
+  }
+
+  function handleCloseDialog() {
+    setOpenDialog(false);
+  }
 
   function handleChangeClass(event) {
     updateClass(charData.id, event.target.value);
-  }
-
-  function renderMenuItems() {
-    return classes.map((characterName) => (
-      <MenuItem value={characterName} key={characterName}>
-        {characterName}
-      </MenuItem>
-    ));
   }
 
   return (
     <React.Fragment>
       {characterEditMode ? (
         <>
-          <Tooltip title="Delete character" placement="top">
-            <Button
-              onClick={() => {
-                removeCharacter(charData.id);
-              }}
-              color="error"
-              fullWidth
-            >
-              <DeleteForeverIcon />
-            </Button>
-          </Tooltip>
-          <FormControl sx={{ m: 0.5, minWidth: 128, width: "1px" }}>
-            {/* <InputLabel id={`character-${charData.id}-label`}>Class</InputLabel> */}
-            <Autocomplete
-              freeSolo
-              options={classes.map((option) => option.label)}
-              value={characterName}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  inputProps={{ ...params.inputProps, maxLength: 16 }}
-                  label="Class"
-                />
-              )}
-              onBlur={handleChangeClass}
-              clearOnEscape
-              disableClearable
-              fullWidth
-            />
-          </FormControl>
+          <Box sx={{ textAlign: "center" }}>
+            <Tooltip title="Delete character" placement="top">
+              <IconButton
+                onClick={() => {
+                  //removeCharacter(charData.id);
+                  handleOpenDialog();
+                }}
+                color="error"
+              >
+                <DeleteForeverIcon />
+              </IconButton>
+            </Tooltip>
+            <FormControl sx={{ m: 0, mt: 1, minWidth: 128, width: "100%" }}>
+              {/* <InputLabel id={`character-${charData.id}-label`}>Class</InputLabel> */}
+              <Autocomplete
+                freeSolo
+                options={classes.map((option) => option.label)}
+                value={characterName}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    inputProps={{ ...params.inputProps, maxLength: 16 }}
+                    label="Class"
+                  />
+                )}
+                onBlur={handleChangeClass}
+                clearOnEscape
+                disableClearable
+                fullWidth
+              />
+            </FormControl>
+          </Box>
+          <Dialog
+            open={openDialog}
+            onClose={handleCloseDialog}
+            aria-labelledby="Delete Character"
+            aria-describedby="Confirm deleting character"
+          >
+            <DialogContent>
+              <DialogContentText>
+                Delete {characterName.length ? characterName : "this character"}
+                ?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseDialog}>Cancel</Button>
+              <Button
+                onClick={() => {
+                  removeCharacter(charData.id);
+                }}
+                autoFocus
+                color="error"
+                variant="outlined"
+              >
+                Delete
+              </Button>
+            </DialogActions>
+          </Dialog>
         </>
       ) : (
         <Typography align="center">{characterName}</Typography>
